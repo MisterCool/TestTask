799e8e9736b0a3c25c56948be534cd960a72c988
Vladislav Bondarev <bond7052@gmail.com>
2018-02-08 23:01:27 +0500

    TestTask

---
+++ Kontur.ImageTransformer/ImageFilter/Filter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterForImage
+{
+    public class Filter
+    {
+        public static void ApplyFilter(Bitmap bmp, string filter)
+        {
+            var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            var bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+            var ptr = bmpData.Scan0;
+
+            int numBytes = bmpData.Width * bmp.Height * 4;
+            byte[] rgbValues = new byte[numBytes];
+
+            Marshal.Copy(ptr, rgbValues, 0, numBytes);
+            switch (filter)
+            {
+                case "sepia":
+                    GetRGBSepia(rgbValues);
+                    break;
+                case "grayscale":
+                    GetRGBGrayScale(rgbValues);
+                    break;
+                default:
+                    var arr = filter.Split('(', ')');
+                    var x = int.Parse(arr[1]);
+                    GetRGBThreshold(rgbValues, x);
+                    break;
+            }
+            Marshal.Copy(rgbValues, 0, ptr, numBytes);
+            bmp.UnlockBits(bmpData);
+        }
+        private static byte[] GetRGBSepia(byte[] array)
+        {
+            for (int i = 0; i < array.Length; i += 4)
+            {
+                int red = array[i + 2];
+                int green = array[i + 1];
+                int blue = array[i + 0];
+
+                array[i + 2] = (byte)Math.Min((.393 * red) + (.769 * green) + (.189 * blue), 255.0);
+                array[i + 1] = (byte)Math.Min((.349 * red) + (.686 * green) + (.168 * blue), 255.0);
+                array[i + 0] = (byte)Math.Min((.272 * red) + (.534 * green) + (.131 * blue), 255.0);   
+            }
+            return array;
+        }
+        private static byte[] GetRGBThreshold(byte[] arrayRGB, int x)
+        {
+            for (int i = 0; i < arrayRGB.Length; i += 4)
+            {
+                int red = arrayRGB[i + 2];
+                int green = arrayRGB[i + 1];
+                int blue = arrayRGB[i + 0];
+                int intensity = red + green + blue;
+                if (intensity >= 255 * x / 100)
+                {
+                    arrayRGB[i + 2] = 255;
+                    arrayRGB[i + 1] = 255;
+                    arrayRGB[i + 0] = 255;
+                }
+                else
+                {
+                    arrayRGB[i + 2] = 0;
+                    arrayRGB[i + 1] = 0;
+                    arrayRGB[i + 0] = 0;
+                }
+            }
+            return arrayRGB;
+        }
+        private static byte[] GetRGBGrayScale(byte[] arrayRGB)
+        {
+            for (int i = 0; i < arrayRGB.Length; i += 4)
+            {
+                int red = arrayRGB[i + 2];
+                int green = arrayRGB[i + 1];
+                int blue = arrayRGB[i + 0];
+                int value = (red + green + blue) / 3;
+
+                arrayRGB[i + 2] = (byte)value;
+                arrayRGB[i + 1] = (byte)value;
+                arrayRGB[i + 0] = (byte)value;
+            }
+            return arrayRGB;
+        }
+    }
+}
---
+++ Kontur.ImageTransformer/Kontur.ImageTransformer/AsyncHttpServer.cs
@@ -0,0 +1,194 @@
+﻿using CheckRequest;
+using FilterForImage;
+using System;
+using System.Collections.Concurrent;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Kontur.ImageTransformer
+{
+    internal class AsyncHttpServer : IDisposable
+    {
+        public AsyncHttpServer()
+        {
+            listener = new HttpListener();
+        }
+
+        public void Start(string prefix)
+        {
+            lock (listener)
+            {
+                if (!isRunning)
+                {
+                    listener.Prefixes.Clear();
+                    listener.Prefixes.Add(prefix);
+                    listener.Start();
+
+                    listenerThread = new Thread(Listen)
+                    {
+                        IsBackground = true,
+                        Priority = ThreadPriority.Highest
+                    };
+                    listenerThread.Start();
+
+                    isRunning = true;
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            lock (listener)
+            {
+                if (!isRunning)
+                    return;
+
+                listener.Stop();
+
+                listenerThread.Abort();
+                listenerThread.Join();
+
+                isRunning = false;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (disposed)
+                return;
+
+            disposed = true;
+
+            Stop();
+
+            listener.Close();
+        }
+      
+        private void Listen()
+        {
+            while (true)
+            {
+                try
+                {
+                    if (listener.IsListening)
+                    {
+                        var context = listener.GetContext();
+                        DelContext del = HandleContextAsync;
+                        var limitRequest = 50;
+                        var limitTask = 10;
+                        if (queueContext.Count() > limitRequest)
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.ServiceUnavailable;
+                            context.Response.Close();
+                        }
+                        if (countTask < limitTask && !queueContext.Any())
+                        {
+                            RequestHandler(del, context);
+                        }
+                        else if (countTask < limitTask && queueContext.Any())
+                        {
+                            queueContext.Enqueue(context);
+                            if (countTask < limitTask && queueContext.Any())
+                                QueueHandler(del, queueContext);
+                        }
+                    }
+                    else Thread.Sleep(0);
+
+                }
+                catch (ThreadAbortException)
+                {
+                    return;
+                }
+                //catch (Exception error)
+                //{
+                //    // TODO: log errors
+                //}
+            }
+        }
+        private static void RequestHandler(DelContext del, HttpListenerContext context)
+        {
+            Interlocked.Increment(ref countTask);
+            var task = Task.Run(() => del.Invoke(context)).ContinueWith(delegate { Interlocked.Decrement(ref countTask); });
+            var timeOfProcessing = TimeSpan.FromMilliseconds(1000);
+            if (!task.Wait(timeOfProcessing))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.ServiceUnavailable;
+                context.Response.Close();
+            }
+        }
+        private static void QueueHandler(DelContext del, ConcurrentQueue<HttpListenerContext> queueContext)
+        {
+            Task.Run(() =>
+            {
+                while (queueContext.TryDequeue(out HttpListenerContext context))
+                {
+                    RequestHandler(del, context);
+                }
+            });
+        }
+        private async Task HandleContextAsync(HttpListenerContext listenerContext)
+        {
+            if (!ValidationRequest.CheckCorrectnessOfTheRequest(listenerContext.Request))
+            {
+                listenerContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                listenerContext.Response.Close();
+            }
+            var request = listenerContext.Request;
+            var stream = request.InputStream;
+            var imageBit = new Bitmap(stream);
+            var url = new Uri(request.Url.ToString());
+            var array = url.Segments;
+            var coordsString = url.Segments.Last();
+            var filter = url.Segments[url.Segments.Count() - 2].Remove(url.Segments[url.Segments.Count() - 2].Length - 1, 1);
+
+            var coordsStringSplit = coordsString.Split(',');
+            var coords = new int[4];
+            for (var i = 0; i < coords.Length; i++)
+                coords[i] = int.Parse(coordsStringSplit[i]);
+
+            switch (filter)
+            {
+                case "sepia":
+                    Filter.ApplyFilter(imageBit, filter);
+                    break;
+                case "grayscale":
+                    Filter.ApplyFilter(imageBit, filter);
+                    break;
+                default:
+                    var arr = filter.Split('(', ')');
+                    var x = int.Parse(arr[1]);
+                    Filter.ApplyFilter(imageBit, filter);
+                    break;
+            }
+
+            var rectangleImage = new Rectangle(0, 0, imageBit.Width, imageBit.Height);
+            var rectangleCoords = new Rectangle(coords[0], coords[1], coords[2], coords[3]);
+
+            var imageInterSect = Rectangle.Intersect(rectangleImage, rectangleCoords);
+            if (imageInterSect == Rectangle.Empty)
+            {
+                listenerContext.Response.StatusCode = (int)HttpStatusCode.NoContent;
+                listenerContext.Response.Close();
+            }
+
+            listenerContext.Response.StatusCode = (int)HttpStatusCode.OK;
+            listenerContext.Response.ContentType = "image/png";
+
+            var iBit = imageBit.Clone(imageInterSect, imageBit.PixelFormat);
+            iBit.Save(listenerContext.Response.OutputStream, ImageFormat.Png);
+            listenerContext.Response.Close();
+        }
+
+        private delegate Task DelContext(HttpListenerContext context);
+        private readonly HttpListener listener;
+        private Thread listenerThread;
+        private bool disposed;
+        private volatile bool isRunning;
+        private static int countTask = 0;
+        private static ConcurrentQueue<HttpListenerContext> queueContext = new ConcurrentQueue<HttpListenerContext>();
+    }
+}
